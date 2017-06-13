65c18dcbbf3d5dab8b202a3c2f6d22681730032b
khjin001010 <khjin001010@naver.com>
2017-06-13 20:36:40 +0900

    #3 init

--- app/src/main/java/khjin001010/kr/hs/emirim/dbex/MainActivity.java
+++ app/src/main/java/khjin001010/kr/hs/emirim/dbex/MainActivity.java
@@ -5,13 +5,46 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button butInit;
+    Button but_insert;
+    Button but_select;
+    EditText edit_group_name;
+    EditText edit_group_count;
+    EditText edit_result_name;
+    EditText edit_result_count;
+    MyDBHelper myDb;
+    SQLiteDatabase sqliteDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        butInit = (Button)findViewById(R.id.but_init);
+        but_insert = (Button)findViewById(R.id.but_insert);
+        but_select = (Button)findViewById(R.id.but_select);
+        edit_group_name = (EditText)findViewById(R.id.edit_group_name);
+        edit_group_count = (EditText)findViewById(R.id.edit_group_count);
+        edit_result_name = (EditText)findViewById(R.id.edit_result_name);
+        edit_result_count = (EditText)findViewById(R.id.edit_result_cout);
+
+        //DB생성
+        myDb = new MyDBHelper(this); // == myHelper 라고 쓰는 것도 좋다(변수명)
+        //기조의 테이블이 존재하면 삭제하고 테이블을 생성한다.
+
+        butInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqliteDb = myDb.getWritableDatabase(); //SQLiteDatabase 참조 값
+                myDb.onUpgrade(sqliteDb,sqliteDb.getVersion(),sqliteDb.getVersion()+1);
+                sqliteDb.close();
+            }
+        });
     }
 
     // 내부 클래스 생성
