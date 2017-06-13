d02ec5ac70655153fb7d12a8084eb1c3670f9c91
khjin001010 <khjin001010@naver.com>
2017-06-13 20:46:41 +0900

    but_insert method

--- app/src/main/java/khjin001010/kr/hs/emirim/dbex/MainActivity.java
+++ app/src/main/java/khjin001010/kr/hs/emirim/dbex/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button butInit;
@@ -41,8 +42,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 sqliteDb = myDb.getWritableDatabase(); //SQLiteDatabase 참조 값
-                myDb.onUpgrade(sqliteDb,sqliteDb.getVersion(),sqliteDb.getVersion()+1);
+                myDb.onUpgrade(sqliteDb,1,2);
                 sqliteDb.close();
+            }
+        });
+
+        but_insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqliteDb = myDb.getWritableDatabase(); //insert문을 실행해야 하니까 writable
+                String sql = "insert into idolTable values('"+edit_group_name.getText()+"', "+edit_group_count.getText()+")";
+                sqliteDb.execSQL(sql);
+                sqliteDb.close();
+                Toast.makeText(MainActivity.this, "저장이 성공적으로 완료되었습니다.", Toast.LENGTH_LONG).show();
             }
         });
     }
